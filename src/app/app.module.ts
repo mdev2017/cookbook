fd9803ae4c0212fb68e012a771bad9f1e7a3639e
Andrey Lukashenko <LukashenkoAndrey@gmail.com>
2018-05-13 18:42:49 +0300

    Introduce routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationMenuComponent,
+    CategoriesComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
